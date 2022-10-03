5167dcc291e98d30ebe1b2dbf49684b021a1e0eb
Siz-007 <108329225+Siz-007@users.noreply.github.com>
2022-10-03 21:26:51 +0545

    Dice

---
+++ index.js
@@ -0,0 +1,27 @@
+let randomNumber1= Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImg= "dice" + randomNumber1 + ".png";
+
+let randomImgSource1= "images/" + randomDiceImg;
+
+let img1= document.querySelectorAll("img")[0];
+
+img1.setAttribute("src" , randomImgSource1);
+
+let randomNumber2= Math.floor(Math.random() *6) + 1;
+
+let randomImgSource2= "images/dice" + randomNumber2 + ".png";
+
+let img2= document.querySelectorAll("img")[1];
+
+img2.setAttribute("src" , randomImgSource2);
+
+if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerHTML= "🚩 Player 1 wins!";
+}
+else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").innerHTML= "Player 2 wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML= "Draw!";
+}
